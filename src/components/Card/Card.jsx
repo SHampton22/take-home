9917eff88c1411c44f510baf73badb38fda672f3
Sarah Hampton <stoluba@gmail.com>
2023-04-12 10:28:58 -0400

    Add Link to card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,12 @@ import './Card.css'
 export default function Card({ id, section, title, image }) {
 
   return (
-    <div className='card' key={id}>
-      <p className='card-category'>{section}</p>
-      <p className='card-title'>{title}</p>
-      <img className='card-thumbnail' src={image}/>
-    </div>
+    // <Link to={`/article-details/${id}`}>
+      <div className='card' key={id}>
+        <p className='card-category'>{section}</p>
+        <p className='card-title'>{title}</p>
+        <img className='card-thumbnail' src={image}/>
+      </div>
+    // </Link>
   )
 }
